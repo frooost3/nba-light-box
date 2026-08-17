28de5a7fba1298894b04576292c4a6a60c108ca1
Snow Zhang <snowzhang03@gmail.com>
2026-08-17 19:06:40 -0400

    Connect NBA score trigger to LED strip

--- scores.py
+++ scores.py
@@ -1,14 +1,41 @@
-import json 
+import json
+import time
+import board
+import adafruit_pixelbuf 
+from adafruit_raspberry_pi5_neopixel_write import neopixel_write
 
 TEAM_COLOURS = {
     "Toronto Raptors": (206, 17, 65),
     "Boston Celtics": (0, 122, 51),
 }
 
+class Pi5Pixelbuf(adafruit_pixelbuf.PixelBuf):
+    def __init__(self, pin, size, **kwargs):
+        self._pin = pin
+        super().__init__(size=size, **kwargs)
+
+    def _transmit(self, buf):
+        neopixel_write(self._pin, buf)
+
+NEOPIXEL = board.D18
+NUM_PIXELS = 60
+
+pixels = Pi5Pixelbuf(
+    NEOPIXEL,
+    NUM_PIXELS,
+    auto_write=True,
+    byteorder="GRB"
+)
+
 def trigger_led(team_name):
     colour = TEAM_COLOURS.get(team_name, (255, 255, 255))
+
     print(f"LED effect triggered for {team_name}")
     print(f"RGB colour: {colour}")
+
+    pixels.fill(colour)
+    time.sleep(2)
+    pixels.fill((0, 0, 0))
 
 def check_score_change(previous_game, current_game):
     if current_game["home_score"] > previous_game["home_score"]:
